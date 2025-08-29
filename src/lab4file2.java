c2e199baca08592dfd8cf0e1a9751f3c8c57fc35
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-29 11:04:59 -0600

    yay

--- src/lab4file2.java
+++ src/lab4file2.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class lab4file2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input: ");
+		String str = scan.nextLine();
 	}
 
 }
---
+++ src/lab4file3.java
@@ -0,0 +1,9 @@
+
+public class lab4file3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
